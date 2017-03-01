bce77a37e4781d67ca7594587540ca536f46f5ad
aaroon84 <aaroon84@hotmail.com>
2017-03-01 19:08:47 +0100

    newserver

--- app.js
+++ app.js
@@ -1,24 +1,44 @@
 /*eslint-env node*/
 
 
+var cfenv = require( 'cfenv' );  
+var express = require( 'express' );  
+var http = require( 'http' );  
+var ws = require( 'ws' );
 
- var WebSocketServer = require('ws').Server;
- 
- var port = (process.env.VCAP_APP_PORT || 8888); 
- 
- wss = new WebSocketServer({port: port});
- wss.on('connection', function(ws) {
-     ws.on('message', function(message) {
-         console.log('received: %s', message);
-         ws.send('echo: ' + message);
-     });
-     ws.send('connected');
- });
+// Environment
+var environment = cfenv.getAppEnv();
 
+// Web
+var app = express();
 
+// Static
+app.use( '/', express.static( 'public' ) );
 
+// Sockets
+var server = http.createServer();  
+var sockets = new ws.Server( {  
+  server: server
+} );
 
+// Listeners
+sockets.on( 'connection', function( client ) {  
+  // Debug
+  console.log( 'Connection.' );
 
+  // Echo messages to all clients
+  client.on( 'message', function( message ) {
+    for( var c = 0; c < sockets.clients.length; c++ ) {
+      sockets.clients[c].send( message );   
+    }
+  } );
+} );
+
+// Start
+server.on( 'request', app );  
+server.listen( environment.port, function() {  
+  console.log( environment.url );
+} );
 
 
 
