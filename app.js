d1f92226e896a01f82d2458873a6f0de08b78a2b
aaroon84 <aaroon84@hotmail.com>
2017-03-28 20:09:04 +0200

    sound test

--- app.js
+++ app.js
@@ -27,6 +27,22 @@ var players = [];
 var player1 = "";
 var player2 = "";
 
+
+var TextToSpeechV1 = require('watson-developer-cloud/text-to-speech/v1');
+var fs = require('fs')
+var text_to_speech = new TextToSpeechV1 ({
+  username: '{b002be7d-4e0d-4b70-bdb9-f33949882f60}',
+  password: '{ogOydPCss8jJ}'
+});
+var params = {
+  text: 'Hello world',
+  voice: 'en-US_AllisonVoice',
+  accept: 'audio/wav'
+};
+text_to_speech.synthesize(params).on('error', function(error) {
+  console.log('Error:', error);
+}).pipe(fs.createWriteStream('hello_world.txt'));
+
 // Listeners
 sockets.on( 'connection', function( client ) {  
   // Debug
